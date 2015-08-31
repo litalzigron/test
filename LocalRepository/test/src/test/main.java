c16e22e9517bcfe3216fefdf56b605a0c35e467a
LitalZigron <litalzigron@gmail.com>
2015-08-31 17:06:04 +0300

    checking

---
+++ LocalRepository/test/src/test/main.java
@@ -0,0 +1,17 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int i;
+		for(i=0;i<5;i++)
+		{
+			System.out.println(i);
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
